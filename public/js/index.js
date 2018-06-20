fe91c00ab33d6cb1c0151e47229092c968f2b64b
AnkanHalder <35003965+AnkanHalder@users.noreply.github.com>
2018-06-20 22:09:08 +0530

    Sending to multiple tabs

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket=io();
+
+socket.on('connect',function(){
+  console.log('Connected');
+
+});
+
+
+socket.on('newMessage',function(message){
+  console.log('New Message',message);
+});
+
+socket.on('disconnect',function(){
+  console.log('Disconnected');
+});
--- server/server.js
+++ server/server.js
@@ -14,14 +14,34 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New User Connected');
 
+  socket.emit('newMessage',{
+    from:'server',
+    text:'Welcome to the chat',
+    createdAt:'Joined '+new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from:'server',
+    text:'New User Joined',
+      createdAt:'Joined '+new Date().getTime()
+  });
+
+
+  socket.on('newMessage',(message)=>{
+    io.emit('newMessage',{
+    from:message.from,
+    text:message.text,
+    createdAt:new Date().getTime()
+  });
+
+  });
+
   socket.on('disconnect',()=>{
     console.log('Disconnected from server');
   });
 
 });
-// io.on('connection',(socket)=>{
-//   console.log('New User Connected');
-// });
+
 
 
 server.listen(3000,()=>{
